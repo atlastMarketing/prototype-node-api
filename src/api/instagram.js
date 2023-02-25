f72ed84b3e43f8f8f27db66ba014ecbe82bd469a
401asfg <mb.allan@ymail.com>
2023-02-25 03:50:29 -0800

    Implemented basic instagram login function (needs to be tested)

--- src/api/instagram.js
+++ src/api/instagram.js
@@ -1,6 +1,28 @@
-const Instagram = require("instagram-web-api");
+const Instagram = require('instagram-web-api');
 
-const client = new Instagram({
-    username: process.env.INSTAGRAM_USERNAME,
-    password: process.env.INSTAGRAM_PASSWORD,
-});
+require("dotenv").config();
+
+const client = new Instagram(
+    {
+        username: process.env.INSTAGRAM_USERNAME,
+        password: process.env.INSTAGRAM_PASSWORD,
+    },
+    {
+        language: "en-US",
+    }
+);
+
+async function login() {
+    console.log("Logging in...");
+
+    await client.login().then(() => {
+        console.log("Login successful!");
+    }).catch((err) => {
+        console.log("Login failed!");
+        console.log(err);
+    })
+}
+
+module.exports = {
+    login,
+};
