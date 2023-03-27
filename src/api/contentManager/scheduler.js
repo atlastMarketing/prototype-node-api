ede91195b5980892d8e6a2575e317b1c9aad7bc6
Jayant <hereisjayant@gmail.com>
2023-03-26 22:54:04 -0700

    Integrated scheduler

--- src/api/contentManager/scheduler.js
+++ src/api/contentManager/scheduler.js
@@ -3,53 +3,48 @@ const CronJob = require('cron').CronJob;
 const Content = require('./models/Content');
 
 
-mongoose.connect('mongodb://localhost/contentManager');
+async function initScheduler(mongo) {
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to database');
+  const db = mongo;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log('Connected to database');
 
-  const scheduleJobs = [];
+    const scheduleJobs = [];
 
-  // Create a function to create cron jobs for all the content items
-  async function createJobs() {
-    const content = await Content.find({ isDraft: false }).exec();
-    
-    // Stop all existing cron jobs
-    scheduleJobs.forEach((job) => job.stop());
-    scheduleJobs.length = 0;
+    // Create a function to create cron jobs for all the content items
+    async function createJobs() {
+      const content = await Content.find({ isDraft: false }).exec();
+      
+      // Stop all existing cron jobs
+      scheduleJobs.forEach((job) => job.stop());
+      scheduleJobs.length = 0;
 
-    // Create new cron jobs for each content item
-    console.log('Creating cron jobs for', content.length, 'content items');
-    content.forEach((item) => {
-      const job = new CronJob(item.postDate, () => {
-        console.log(`Posting ${item.caption} to ${item.socialMedia}`);
+      // Create new cron jobs for each content item
+      console.log('Creating cron jobs for', content.length, 'content items');
+      content.forEach((item) => {
+        const job = new CronJob(item.postDate, () => {
+          console.log(`Posting ${item.caption} to ${item.socialMedia}`);
+        });
+        scheduleJobs.push(job);
+        job.start();
+        console.log(`Cron job created for ${item.caption}`);
       });
-      scheduleJobs.push(job);
-      job.start();
-      console.log(`Cron job created for ${item.caption}`);
+    }
+
+    // Create cron jobs for the initial set of content items
+    createJobs();
+
+    // Create a cron job to update the scheduled jobs every 5 seconds
+    const updateJobsCron = new CronJob('*/5 * * * * *', async () => {
+      console.log('Updating scheduled jobs');
+      await createJobs();
     });
-  }
 
-  // Create cron jobs for the initial set of content items
-  createJobs();
+    // Start the updateJobsCron cron job
+    updateJobsCron.start();
 
-  // Listen for changes in the Content model and update the cron jobs accordingly
-    // This is not working yet
-//   Content.watch().on('change', (change) => {
-//     console.log('Database change detected:');
-//     createJobs();
-//   });
-
-// this is a workaround for the above code not working
-  // Create a cron job to update the scheduled jobs every 15 seconds
-  const updateJobsCron = new CronJob('*/15 * * * * *', async () => {
-    console.log('Updating scheduled jobs');
-    await createJobs();
   });
+}
 
-  // Start the updateJobsCron cron job
-  updateJobsCron.start();
-
-});
+module.exports = { initScheduler };
